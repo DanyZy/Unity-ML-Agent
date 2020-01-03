82bb57cf5203f04b7439dcc688ec769ac2dde4e5
DanyZy <daniilzgurovec@gmail.com>
2020-01-03 22:34:15 +0600

    movement implementation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isGrounded = false;
+
+    public float speed = 10.0f;
+    public float jumpForce = 5.0f;
+
+    private void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(float verticalInput, float horizontalInput)
+    {
+        Vector3 movementVector = new Vector3(verticalInput, 0.0f, horizontalInput);
+        rb.AddForce(movementVector * speed, ForceMode.Acceleration);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    #region Collision handler
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        playerController.Move(xInput, zInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerController.Jump();
+        }
+    }
+}
