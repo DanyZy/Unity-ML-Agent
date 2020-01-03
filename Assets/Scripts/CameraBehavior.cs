13646b26438feb421773b66a8830f0026d5027f1
DanyZy <daniilzgurovec@gmail.com>
2020-01-04 00:29:53 +0600

    implemented camera behavior & added some textures

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed = 10.0f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            target.position + offset, 
+            followSpeed * Time.deltaTime
+        );
+    }
+}
